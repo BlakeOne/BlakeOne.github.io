aab947976b179b237520fd33e422b7bac306a789
BlakeOne <30273164+BlakeOne@users.noreply.github.com>
2021-09-16 14:27:48 -0700

    v1c

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
     BABYLON.NodeEditor.Show({
         hostElement: document.getElementById("nmeDiv"),
-        nodeMaterial: new BABYLON.NodeMaterial("node", scene, { emitComments: true })
+        nodeMaterial: new BABYLON.NodeMaterial("node", null, { emitComments: true })
     });
 });
